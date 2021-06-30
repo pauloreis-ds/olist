b80054ec16b989947b06ec0754dfd50bb9d9426f
pauloreis-ds <phsabino.reis@gmail.com>
2021-06-30 12:34:32 -0300

    removing unnecessary query.sql

--- business_questions_insights/sql_queries/total_sales_per_month.sql
+++
@@ -1,9 +0,0 @@
-
-           SELECT date(MIN(order_purchase_timestamp)), SUM(price)
-           FROM   orders LEFT JOIN order_items ON orders.order_id = order_items.order_id
-           WHERE  order_status='delivered' and
-                  order_purchase_timestamp BETWEEN date('{intial_date}') AND date('{final_date}')
-           
-           
-           ;
-        
