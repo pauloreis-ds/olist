79000facd381d030294e7f64931aa16b6061f546
pauloreis-ds <phsabino.reis@gmail.com>
2021-07-13 19:24:32 -0300

    sales_forecast init

---
+++ olistlib/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name="olistlib",
+    version="0.1",
+    author="Paulo Reis",
+    author_email="pauloreis.datascience@gmail.com",
+    description="olist useful module",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+)
