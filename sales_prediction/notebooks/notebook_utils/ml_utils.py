c838ac14f242d0b966b4f6623e4a0ef9443ce8f1
pauloreis-ds <phsabino.reis@gmail.com>
2021-07-26 16:25:18 -0300

    sales time series analysis and forecast

--- sales_prediction/notebooks/notebook_utils/ml_utils.py
+++ sales_prediction/notebooks/notebook_utils/ml_utils.py
@@ -1,9 +1,11 @@
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 
 
-def evaluation(y_true, y_pred, show_results=True):
+def evaluation(y_true, y_pred, timeline_message=None, show_results=True):
     mae = mean_absolute_error(y_true, y_pred).round(2)
     mape = mean_absolute_percentage_error(y_true, y_pred) * 100
+    if timeline_message:
+        print(timeline_message)    
     if show_results:
         print(f'''On average, our predictions are {mape.round(2)}% above or below the real value.''')
         print(f'''Which means an error of about R$ {mae}''')
